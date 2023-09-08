83843673ec9e21fa95222a9f06d4ff3ff1372009
Mdumisi Kelvin Letsie <mdu.letsie7@gmail.com>
2023-09-08 22:03:01 +0200

    first commit

---
+++ app/src/main/java/com/example/shuttlesmartapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.shuttlesmartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnShuttleSchedules = findViewById(R.id.btnShuttleSchedules);
+        Button btnDriverProfiles = findViewById(R.id.btnDriverProfiles);
+        Button btnUserFeedback = findViewById(R.id.btnUserFeedback);
+
+        btnShuttleSchedules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openShuttleSchedules();
+            }
+        });
+
+        btnDriverProfiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDriverProfiles();
+            }
+        });
+
+        btnUserFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDriverProfiles();
+            }
+        });
+
+        btnUserFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUserFeedback();
+            }
+        });
+
+    }
+
+    public void openShuttleSchedules() {
+        Intent intent = new Intent(this, ShuttleSchedules.class);
+        startActivity(intent);
+    }
+
+    public void openDriverProfiles() {
+        Intent intent = new Intent(this, DriversProfiles.class);
+        startActivity(intent);
+    }
+
+    public void openUserFeedback() {
+        Intent intent = new Intent(this, UserFeedback.class);
+        startActivity(intent);
+    }
+}
